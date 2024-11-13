fe9cc529bfb10066d3ccb078d1c588f134539fad
Kaburi <specialemail90@gmail.com>
2024-11-13 16:23:24 +0900

    .

--- src/pages/auth/ui/LoginPage.tsx
+++ src/pages/auth/ui/LoginPage.tsx
@@ -1,9 +1,12 @@
 import { LoginForm } from "@/features/auth/ui/login-form";
+import { Env } from "@/shared/env";
 
 export const LoginPage = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-50">
       <LoginForm />
+      {Env.HEADER_KEY}
+      {Env.HEADER_VALUE}
     </div>
   );
 };
